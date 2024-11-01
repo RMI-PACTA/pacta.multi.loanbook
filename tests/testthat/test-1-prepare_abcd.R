b6d5925b509670e0e9d91715b58961f539168585
CJ Yetman <cj@cjyetman.com>
2024-11-01 12:51:10 +0100

    Integration testing (#185)

---
+++ tests/testthat/test-1-prepare_abcd.R
@@ -0,0 +1,29 @@
+test_that("with known input, runs without error", {
+  config <-
+    list(
+      directories = list(
+        dir_input = test_path("test-data", "input"),
+        dir_prepared_abcd = test_path("test-data", "output", "prepared_abcd")
+      ),
+      file_names = list(
+        filename_abcd = "ABCD.xlsx",
+        sheet_abcd = "Company Indicators - PACTA Comp"
+      ),
+      project_parameters = list(
+        start_year = 2022L,
+        time_frame = 5L
+      ),
+      sector_split = list(
+        apply_sector_split = FALSE,
+        sector_split_type = "equal_weights",
+        filename_split_company_id = "split_company_ids.csv",
+        filename_advanced_company_indicators = "test-cmpy-indicators.xlsx",
+        sheet_advanced_company_indicators = "Company Activities"
+      ),
+      prepare_abcd = list(
+        remove_inactive_companies = TRUE
+      )
+    )
+  
+  expect_no_condition(prepare_abcd(config))
+})
---
+++ tests/testthat/test-2-match_loanbooks.R
@@ -0,0 +1,25 @@
+test_that("with known input, runs without error", {
+  config <-
+    list(
+      directories = list(
+        dir_input = test_path("test-data", "input"),
+        dir_prepared_abcd = test_path("test-data", "output", "prepared_abcd"),
+        dir_matched_loanbooks = test_path("test-data", "output", "matched_loanbooks")
+      ),
+      matching = list(
+        params_match_name = list(
+          by_sector = TRUE,
+          min_score = 0.9,
+          method = "jw",
+          p = 0.1,
+          overwrite = NULL,
+          join_id = NULL
+        ),
+        manual_sector_classification = list(
+          use_manual_sector_classification = FALSE
+        )
+      )
+    )
+
+  expect_no_error(suppressMessages(match_loanbooks(config)))
+})
