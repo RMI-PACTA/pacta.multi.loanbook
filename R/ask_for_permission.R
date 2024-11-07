82c43a4540a241564172c7b66831662c1439601d
CJ Yetman <cyetman@rmi.org>
2024-11-07 23:05:44 +0100

    report from the calling env/func

--- R/ask_for_permission.R
+++ R/ask_for_permission.R
@@ -1,6 +1,9 @@
 ask_for_permission <- function(msg) {
   if (!yesno::yesno(cli::cli_text(msg))) {
-    cli::cli_abort(message = "The process cannot continue because you answered \"no\"")
+    cli::cli_abort(
+      message = "The process cannot continue because you answered \"no\"",
+      call = rlang::caller_env()
+    )
   }
   
   invisible(TRUE)
