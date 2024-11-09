002c7b3eb89de44ffc44cbed0c18fac2c0575c8c
CJ Yetman <cj@cjyetman.com>
2024-11-09 12:09:12 +0100

    process `msg` in `caller_env` (#222)

--- R/ask_for_permission.R
+++ R/ask_for_permission.R
@@ -1,5 +1,5 @@
 ask_for_permission <- function(msg) {
-  if (!yesno::yesno(cli::cli_text(msg))) {
+  if (!yesno::yesno(cli::cli_text(msg, .envir = rlang::caller_env()))) {
     cli::cli_abort(
       message = "The process cannot continue because you answered \"no\"",
       call = rlang::caller_env()
