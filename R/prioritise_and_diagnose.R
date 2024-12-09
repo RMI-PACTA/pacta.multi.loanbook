83842f2eb3b9407b72676f7a11a9234a85531482
CJ Yetman <cj@cjyetman.com>
2024-12-09 12:52:14 +0100

    add examples and return value explanations to exported functions (#311)

--- R/prioritise_and_diagnose.R
+++ R/prioritise_and_diagnose.R
@@ -1,19 +1,29 @@
 #' Prioritise and diagnose the loan book data sets used in the PACTA for Supervisors analysis
 #'
 #' @description
-#' `prioritise_and_diagnose()` runs the necessary steps to prioritise the matched
-#' loan books and diagnose both the match success rate and the coverage of the
-#' real economy assets by the matched loan books.
-#' Parameters for all steps are read from a `config.yml` file. The function is
-#' called for its side effects and writes the prepared and diagnosed data sets
-#' to the directory `output/prioritise_and_diagnose`, where `output` is the
-#' output directory  specified in the `config.yml`.
+#' `prioritise_and_diagnose()` runs the necessary steps to prioritise the
+#' matched loan books and diagnose both the match success rate and the coverage
+#' of the real economy assets by the matched loan books. Parameters for all
+#' steps are read from a `config.yml` file. The function is called for its side
+#' effects and writes the prepared and diagnosed data sets in the directory
+#' specified by `dir_prioritized_loanbooks_and_diagnostics` in the `config.yml`.
 #'
 #' `prioritise_and_diagnose()` and `prioritize_and_diagnose()` are synonyms.
 #'
 #' @param config either a path to a config.yml file or a list of parameters
 #'
-#' @return NULL
+#' @return
+#'
+#' `prioritise_and_diagnose()` returns `NULL` invisibly. The function is called
+#' for its side effects and writes the prepared and diagnosed data sets in the
+#' directory specified by `dir_prioritized_loanbooks_and_diagnostics` in the
+#' `config.yml`.
+#'
+#' @examples
+#' \dontrun{
+#' config <- "path/to/config.yml"
+#' prioritise_and_diagnose(config)
+#' }
 #'
 #' @export
 
